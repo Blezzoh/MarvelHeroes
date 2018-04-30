4bbf4a5218e42e44b3c405250052aeb42d61e2d8
Blaise Iradukunda <iradub0@sewanee.edu>
2018-04-29 21:17:18 -0500

    beginning

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Spiderman', actor:'Peter Parker' },
+  { id: 12, name: 'Hulk', actor: 'Robert Bruce Banner' },
+  { id: 13, name: 'captain Marvel', actor: 'Carol Danvers' },
+  { id: 14, name: 'Iron Man', actor: 'Robert D. Junior' },
+  { id: 15, name: 'Black Widow', actor: 'Scarlett Johansson' },
+  { id: 16, name: 'Luke Cage', actor: 'Mke Colter' },
+  { id: 17, name: 'Captain America', actor: 'Chris Evans'}
+];
